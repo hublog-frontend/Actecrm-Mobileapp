5d95413ef5eeedac9de49f53fcb5607d4b1aa27d
balaji_hublog <176136710+hublog-frontend@users.noreply.github.com>
2026-05-19 09:55:43 +0530

    removed old project

--- android/app/.cxx/Debug/293u281e/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "D:/Acte-Crm-Mobile-App/Actecrm/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/3q31r4o5/obj/x86_64/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "D:/Acte-Crm-Mobile-App/Actecrm/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
